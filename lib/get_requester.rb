2398a35c366011d4c9308aafd21f5db1e0586144
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-31 11:41:18 -0800

    commit tree 12241a1a4a4d5874e0d38c9330e5f99f574c8477

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+#  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json    
+    people = JSON.parse(self.get_response_body)
+    result = people.collect do |person|
+        { "name" => person["name"],
+          "occupation" => person["occupation"] }
+    end
+    result
+  end
+end
